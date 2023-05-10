fdfaa94521cd84bc769887f61b37cd46b02b72d5
VolodymyrShah <vova21sah@gmail.com>
2023-05-10 23:53:09 +0300

    add-route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-
 import "./App.css";
-import Header from './components/Header';
-
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Blog from "./pages/Blogs";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 function App() {
   return (
-   <div>
-   <Header />
-   <h1>Hello World</h1>
- </div>
+    <div>
+      <Header />
+      <Container>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/blogs" element={<Blog />} />
+        </Routes>
+      </Router>
+      </Container> 
+    </div>
   );
 }
 
